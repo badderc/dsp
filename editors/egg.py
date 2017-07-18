855422c8d742f0970f440cdae58cd344266715e1
Carlie Badder <badderc@gmail.com>
2017-07-18 10:19:09 -0700

    Added three files from three different editors

---
+++ editors/egg.py
@@ -0,0 +1,3 @@
+print("This file was created using emacs")
+print("emacs is the third text editor I have tested out today")
+print("I will probably use nano or vim to start")
---
+++ editors/vinegar.py
@@ -0,0 +1,3 @@
+print("This file was created using the vim editor")
+print("Vim Adventures helped me learn a few things about navigating vim")
+
